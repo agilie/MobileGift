58959f67ba659b9cca6cc669105e0dcc3b105f02
Eugene Surkov <eugene.surkov@agilie.com>
2017-04-07 17:13:00 +0300

    init MobileEasterGift project

---
+++ app/src/main/java/com/agilie/mobileeastergift/AGMobileEasterGiftInterface.kt
@@ -0,0 +1,5 @@
+package com.agilie.mobileeastergift
+
+
+interface AGMobileEasterGiftInterface {
+}
---
+++ app/src/main/java/com/agilie/mobileeastergift/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.agilie.mobileeastergift
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
+
+
