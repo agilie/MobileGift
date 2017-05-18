5f4b2fa8e31f4959dd361bf895d7d5f613f58ce9
Eugene Surkov <eugene.surkov@agilie.com>
2017-05-18 10:15:04 +0300

    implament shake with animation set

--- AGMobileGiftInterface-Example/src/main/java/com/agilie/mobileeastergift/ui/MainActivity.kt
+++ AGMobileGiftInterface-Example/src/main/java/com/agilie/mobileeastergift/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.agilie.mobileeastergift.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
 import com.agilie.agmobilegiftinterface.AGMobileGiftInterfaceImpl
 import com.agilie.mobileeastergift.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,11 +26,29 @@ class MainActivity : AppCompatActivity() {
         gravity.setOnClickListener { startActivity(GravityActivity.getCallingIntent(this)) }
 
 
+        var animatorSet = AnimatorSet()
 
-        var anim = AnimationUtils.loadAnimation(this, R.anim.shake)
+        var objectRotateAnimator = ObjectAnimator.ofFloat(shake, "rotation", -5f, 5f)
+        objectRotateAnimator.setValues()
+        objectRotateAnimator.apply {
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+            duration = 70
+            interpolator = LinearInterpolator()
+        }
 
+        var objectTranslateAnimator = ObjectAnimator.ofFloat(shake, "translate", -5f, 5f)
+        objectTranslateAnimator.apply {
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+            duration = 70
+            interpolator = LinearInterpolator()
+        }
 
-        start_shake.setOnClickListener { shake.startAnimation(anim) }
+        start_shake.setOnClickListener {
+            animatorSet.play(objectRotateAnimator).with(objectTranslateAnimator)
+            animatorSet.start()
+        }
 
 
     }
